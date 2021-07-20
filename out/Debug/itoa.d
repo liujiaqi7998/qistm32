3748e86442ce7d6ada5f977ad8261ceb6caf65e3
liujiaqi <1792498113@qq.com>
2021-07-20 14:25:20 +0800

    20210720

--- out/Debug/itoa.d
+++ out/Debug/itoa.d
@@ -1,3 +1,3 @@
-.\out\Debug\itoa.o: .\ArduinoAPI\itoa.c
-.\out\Debug\itoa.o: .\ArduinoAPI\itoa.h
+.\out\Debug\itoa.o: .\lib\ArduinoAPI\itoa.c
+.\out\Debug\itoa.o: .\lib\ArduinoAPI\itoa.h
 .\out\Debug\itoa.o: C:\Keil_v5\ARM\ARMCC\bin\..\include\string.h
--- out/Debug/new.d
+++ out/Debug/new.d
@@ -1,3 +1,3 @@
-.\out\Debug\new.o: .\ArduinoAPI\new.cpp
-.\out\Debug\new.o: .\ArduinoAPI\new.h
+.\out\Debug\new.o: .\lib\ArduinoAPI\new.cpp
+.\out\Debug\new.o: .\lib\ArduinoAPI\new.h
 .\out\Debug\new.o: C:\Keil_v5\ARM\ARMCC\bin\..\include\stdlib.h
